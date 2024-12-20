570f177d04ee1bbee3c2ec5d4db7cf738dd7f0f7
Mac Chic <mharrvic@gmail.com>
2024-12-20 13:54:05 +0800

    Update

--- src/infra/lambda/api.ts
+++ src/infra/lambda/api.ts
@@ -1,27 +1,20 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
-import { db } from "~/db/drizzle";
-import { todo } from "~/db/todo.sql";
+// import { db } from "~/db/drizzle";
 
 export const handler = async (evt: APIGatewayProxyEventV2) => {
   if (evt.requestContext.http.method === "GET") {
-    const result = await db.select().from(todo).execute();
+    // const result = await db.select().from(todo).execute();
 
     return {
       statusCode: 200,
-      body: JSON.stringify(result, null, 2),
+      body: JSON.stringify("test", null, 2),
     };
   }
 
   if (evt.requestContext.http.method === "POST") {
-    const result = await db
-      .insert(todo)
-      .values({ title: "Todo", description: crypto.randomUUID() })
-      .returning()
-      .execute();
-
     return {
       statusCode: 200,
-      body: JSON.stringify(result),
+      body: JSON.stringify("test", null, 2),
     };
   }
 };
