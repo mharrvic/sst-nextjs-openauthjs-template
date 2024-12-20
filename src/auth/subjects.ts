4352d28eeb96f231688e2ac4bd83c5fb12bd4956
Mac Chic <mharrvic@gmail.com>
2024-12-20 13:55:45 +0800

    Update

--- src/auth/subjects.ts
+++ src/auth/subjects.ts
@@ -2,6 +2,7 @@ import { createSubjects } from "@openauthjs/openauth";
 import { object, string } from "valibot";
 
 export const subjects = createSubjects({
+  // @ts-expect-error - Overriding the default subjects
   user: object({
     id: string(),
   }),
