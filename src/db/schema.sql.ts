28079a074e1e10c192cc59670a6abb52c4afb8c7
Mac Chic <mharrvic@gmail.com>
2024-12-20 13:54:19 +0800

    Add schema

---
+++ src/db/schema.sql.ts
@@ -0,0 +1,13 @@
+import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 100 }),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  role: varchar("role", { length: 20 }).notNull().default("member"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  lastSigninAt: timestamp("last_signin_at"),
+  lastSignoutAt: timestamp("last_signout_at"),
+  deletedAt: timestamp("deleted_at"),
+});
