4c393a30dc21f0cd058539721a018c67ba70b03e
Mac Chic <mharrvic@gmail.com>
2024-12-20 13:54:48 +0800

    Add token endpoint

---
+++ src/app/api/token/route.ts
@@ -0,0 +1,18 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { setTokens } from "~/app/auth";
+
+export async function GET(req: NextRequest) {
+  try {
+    const url = new URL(req.url);
+
+    const accessToken = url.searchParams.get("access_token") ?? "";
+    const refreshToken = url.searchParams.get("refresh_token") ?? "";
+    console.log({ accessToken, refreshToken });
+
+    await setTokens(accessToken, refreshToken);
+    return NextResponse.redirect(`${url.origin}/`);
+  } catch (error) {
+    console.error({ error });
+    return NextResponse.json({ error });
+  }
+}
