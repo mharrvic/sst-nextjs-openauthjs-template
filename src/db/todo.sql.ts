c0904c5b3ec6bbf60010ce0750b9b62aac1cec4f
Mac Chic <mharrvic@gmail.com>
2024-12-20 13:54:11 +0800

    Remove todo

--- src/db/todo.sql.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
-
-export const todo = pgTable("todo", {
-  id: serial("id").primaryKey(),
-  title: text("title").notNull(),
-  description: text("description"),
-});
